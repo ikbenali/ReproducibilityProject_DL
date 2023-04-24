Created notebook versions of scripts for easy playground adjustments. Also reverted the Poisson1DNTK script to previous commit as adjustements were producing wrong results

Implementation_1/Poisson/Poisson1D_NTK.py
@@ -5,7 +5,6 @@ Reproducbility project
 
 import numpy as np
 import matplotlib.pyplot as plt
-from pathlib import Path
 
 # Pytorch
 import torch
@@ -15,12 +14,17 @@ from torch.utils.data import DataLoader, RandomSampler
 import torchinfo
 
 ### Own modules
-from src.PDE import Poisson1D
-from src.PINN import PINN
-from src.plotFunctions import plot_results1D, plot_NTK, plot_param_ntk_diff, plot_NTK_change, plot_convergence_rate
+### Own modules
+import sys
+sys.path.insert(0, '../src/')
+
+from PDE import Poisson1D
+from PINN import PINN
+from plotFunctions import plot_results1D, plot_NTK, plot_param_ntk_diff, plot_NTK_change, plot_convergence_rate
 
 ### Set dtype and device to be used
 dtype = torch.float32
+
 
 save_model      = True
 train_model     = True
@@ -32,8 +36,6 @@ elif torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
-    
-# device = torch.device('cpu')
 
 ### Define Poisson1D Exact, forcing function and boundary condition
 def f_u_exact(a,x):
@@ -80,8 +82,7 @@ NX  = 100
 dx = (X_N - X_0) / NX
 
 # Create points for interior and boundary
-
-Xr = torch.zeros(NX,1, dtype=dtype, device=device).uniform_(X_0, X_N).requires_grad_(True)
+Xr = torch.linspace(X_0, X_N, NX, dtype=dtype, device=device, requires_grad=True).view(-1,1)
 Xb = torch.randint(0, 2, (NX,1),  dtype=dtype, device=device, requires_grad=True)
 X  = torch.hstack((Xr, Xb))
 
@@ -104,15 +105,15 @@ neurons     = [100]
 net         = PINN(input_size, output_size, neurons, PDE, dtype, device, log_parameters, log_NTK)
 net.to(device)
 
-torchinfo.summary(net, input_size=(Br, 1), device=device)
+torchinfo.summary(net, input_size=(Br, 1))
 
 # Training parameters
 size          = len(XTrain.dataset)
 learning_rate = 1e-5
-epochs        = int(10e3)
+epochs        = int(40e3)
 
-optimizer = optim.SGD
-# optimizer = optim.Adam
+optimizer = optim.SGD(net.parameters(), learning_rate)
+# optimizer = optim.Adam(net.parameters(), learning_rate)
 
 ##################### Train network
 
@@ -129,51 +130,41 @@ if use_adaptation_algorithm:
 else:
     model_adaption = ''
 
-if optimizer == optim.SGD:
-    opt = 'SGD'
-elif optimizer == optim.Adam:
-    opt = 'Adam'
-
-file_name = f'{model_name}_Epoch={epochs}_Optimizer={opt}{model_adaption}'
+file_name = f'{model_name}_{epochs}{model_adaption}'
 path      = './output/models/'
 pathfile  = path+file_name
-Path(path).mkdir(parents=True, exist_ok=True)
+
 
 #### Train loop
 train_losses = []
 
 # Auto Mixed Precision settings
-use_amp = True
-if device == torch.device('cpu'):
-    use_amp = False
+use_amp = False
 scaler  = torch.cuda.amp.GradScaler(enabled=use_amp)
 
-optimizer = optimizer(net.parameters(), learning_rate)
-
-## Observe initial estimation of NTK Matrix
-if compute_NTK:
-    ## Observe initial estimation of NTK Matrix
-    net.eval()
-    x       = next(iter(XTrain)).view(-1, Br, 1)
-    x_prime = next(iter(XTrain)).view(-1, Br, 1)
-
-    net.NTK(x, x_prime)
-
-    if log_NTK:
-        net.log_NTK(0)
-    
-    # reset lambda
-    net.lambda_adaptation = [1., 1.]
 
 if train_model:
+
     for epoch in range(epochs+1):
-        net.train()
+
+        if epoch == 0 and compute_NTK:
+            ## Observe initial estimation of NTK Matrix
+            net.eval()
+            x       = next(iter(XTrain)).view(-1, Br, 1)
+            x_prime = next(iter(XTrain)).view(-1, Br, 1)
+
+            net.NTK(x, x_prime)
+            if log_NTK:
+                net.log_NTK(0)
+            # reset lambda
+            # net.lambda_adaptation = torch.tensor([1., 1.], dtype=dtype, device=device)
 
         # log parameters and set in training mode
-        if log_parameters:
-            net.log_parameters(epoch)
+        net.log_parameters(epoch)
+        net.train()
 
         epoch_loss   = 0.0
+
         for i, x in enumerate(XTrain):
             # reset gradients
             optimizer.zero_grad()
@@ -215,13 +206,10 @@ if train_model:
                     x_prime  = x
 
             # Do optimisation step
-            if use_amp:
-                scaler.scale(net.loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                net.loss.backward()
-                optimizer.step()
+            scaler.scale(net.loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
         ### END Batch loop
 
         # Compute NTK
@@ -233,37 +221,31 @@ if train_model:
 
                 if log_NTK:
                     net.log_NTK(epoch)
-                    net.save_log(pathfile)
 
         train_losses.append(epoch_loss / len(XTrain))
         
         if epoch % 100 == 0 or epoch == epochs: 
-            print(f"Epoch: {epoch:4d}     loss: {train_losses[-1]:5f}")
+            print(f"Epoch: {epoch:4d}     Loss: {train_losses[-1]:4f}   Lr: {optimizer.param_groups[0]['lr']:.2E}")
+
+            if use_adaptation_algorithm:
+                lambda_weights = ""
+                for lambda_i in net.lambda_adaptation:
+                    lambda_weights += f"{lambda_i.item():5f} "
+                print(f"                Lambda Adaption: " + lambda_weights)
     ### END training loop
 
     #### save model
     if save_model:
         net.save_model(pathfile)
         net.save_log(pathfile)
-        with open(f'{pathfile}.npy', 'wb') as f:
-            np.save(f, np.array(train_losses))
 
 #%% 
 ### Plot Results
 
-read_model = True
-
-if read_model:
-    net.read_model(pathfile)
-    net.read_log(pathfile)
-    with open(f'{pathfile}.npy', 'rb') as f:
-        train_losses = np.load(f)
-
-path      = './output/figures/'
-pathfile  = path+file_name
-Path(path).mkdir(parents=True, exist_ok=True)
-
 net.eval()
+net.read_model(pathfile)
+net.read_log(pathfile)
+
 NX = 100
 
 xplot = torch.linspace(X_0, X_N, NX, dtype=dtype).view(-1,1).to(device)
@@ -279,27 +261,22 @@ u_pred  = u_pred.cpu().detach().numpy()
 ## Plot 1 - Prediction and training loss
 fig1, axs1 = plot_results1D(xplot, u_pred, u_exact, train_losses)
 fig1.suptitle(f'Poisson 1D - a = {a} Width = {neurons}')
-plt.savefig(pathfile+'_plot_1D')
 
 # Plot 2 - Parameter and ntk difference
 fig2, axs2 = plt.subplots(1,2, figsize=(18,6))
 plot_param_ntk_diff(net, fig2, axs2)
-plt.savefig(pathfile+'_plot_param_ntk_diff')
 
 # Plot 3 - Plot all NTK matrices
 fig3, axs3 = plt.subplots(1,3, figsize=(18,6))
 plot_NTK(net, fig3, axs3)
-plt.savefig(pathfile+'_plot_NTK')
 
 # Plot 4 - NTK matrix K change
 fig4, axs4 = plt.subplots(1,1)
 plot_NTK_change(net, fig4, axs4)
-plt.savefig(pathfile+'_plot_NTK_change')
 
 # Plot 5 - Convergence rate for all matrices
 fig5, axs5 = plt.subplots(1,1)
 plot_convergence_rate(net, fig5, axs5)
-plt.savefig(pathfile+'_plot_convergence_rate')
 
 plt.show()
 

Implementation_1/Poisson/ReproducibilityResults.py
@@ -11,9 +11,13 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, RandomSampler
 
 ### Own modules
-from src.PDE import Poisson1D
-from src.PINN import PINN
-from src.plotFunctions import plot_param_ntk_diff, plot_NTK_change, plot_convergence_rate
+### Own modules
+import sys
+sys.path.insert(0, '../src/')
+
+from PDE import Poisson1D
+from PINN import PINN
+from plotFunctions import plot_param_ntk_diff, plot_NTK_change, plot_convergence_rate
 
 ### Set dtype and device to be used
 dtype = torch.float32
@@ -24,6 +28,8 @@ elif torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
+
+# device = torch.device('cpu')
 
 ### Define Poisson1D Exact, forcing function and boundary condition
 def f_u_exact(a,x):
@@ -40,7 +46,7 @@ def f_x(a, x):
     """
     fx = -(a**2)*(torch.pi**2)*torch.sin(a*torch.pi*x)
        
-    return fx
+    return fx.view(-1,1)
 
 def g_x(x, xb):
     """
@@ -55,7 +61,7 @@ def g_x(x, xb):
     ub[xb1_idx] = 0
     ub[xb2_idx] = 0
 
-    return ub
+    return ub.view(-1,1)
 
 ###### Setup PINN 
 
@@ -70,13 +76,13 @@ dx = (X_N - X_0) / NX
 # Create points for interior and boundary
 Xr = torch.linspace(X_0, X_N, NX, dtype=dtype, device=device, requires_grad=True).view(-1,1)
 Xb = torch.randint(0, 2, (NX,1),  dtype=dtype, device=device, requires_grad=True)
-X  = torch.stack((Xr, Xb), dim=0)
+X  = torch.hstack((Xr, Xb))
 
 ### Setup PINN Network
-Nr      = 100
-Nb      = 100
+Br      = 100
+Bb      = 100
 rand_sampler = RandomSampler(X, replacement=True)
-XTrain       = DataLoader(X, Nr ,sampler=rand_sampler)
+XTrain       = DataLoader(X, batch_size=Br, shuffle=True)
 
 # net parameters
 input_size  = 1
@@ -102,11 +108,17 @@ for net_i in neural_nets:
 X       = next(iter(XTrain))
 X_prime = next(iter(XTrain))
 
+xr       = X[:,0].to(device).view(-1,1);       xb       = X[:,1].to(device).view(-1,1)
+xr_prime = X_prime[:,0].to(device).view(-1,1); xb_prime = X_prime[:,1].to(device).view(-1,1)
+
+X = torch.stack([xr, xb], dim=0);      X_prime = torch.stack([xr_prime, xb_prime], dim=0)
+
 ### PLOT Eigenvalue of NTK matrices
 fig, axs = plt.subplots(1,3, figsize=(23,6))
 ylabels = [r'$\lambda_{K}$', r'$\lambda_{uu}$', r'$\lambda_{rr}$']
 
 for i in range(len(a)):
+
     neural_nets[i].NTK(X, X_prime)
 
     eig_K_plot    = np.sort(np.real(neural_nets[i].lambda_K.detach().cpu().numpy()))[::-1]
@@ -117,12 +129,12 @@ for i in range(len(a)):
     axs[1].semilogx(eig_K_uu_plot,   label=f'a={a[i]}');    axs[1].set_title('Eigenvalue of {}'.format(r"$K_{uu}$"))
     axs[2].semilogx(eig_K_rr_plot,   label=f'a={a[i]}');    axs[2].set_title('Eigenvalue of {}'.format(r"$K_{rr}$"))
 
-    for ax in axs:
-        ax.legend()
-        # ax.ticklabel_format(axis='y', scilimits=(0,0))
-        ax.set_yscale('log')
-        ax.set_ylabel(ylabels[i])
-        ax.set_xlabel(r'$Index$')
+for i,ax in enumerate(axs):
+    ax.legend()
+    # ax.ticklabel_format(axis='y', scilimits=(0,0))
+    ax.set_yscale('log')
+    ax.set_ylabel(ylabels[i])
+    ax.set_xlabel(r'$Index$')
 
 plt.show()
 
@@ -137,8 +149,14 @@ log_NTK         = True
 
 neural_nets  = [PINN(input_size, output_size, neurons_i, PDE, dtype, device, log_parameters=log_parameters, log_NTK=log_NTK) for neurons_i in neurons];
 
-x       = next(iter(XTrain))
-x_prime = next(iter(XTrain))
+X       = next(iter(XTrain))
+X_prime = next(iter(XTrain))
+
+xr       = X[:,0].to(device).view(-1,1);       xb       = X[:,1].to(device).view(-1,1)
+xr_prime = X_prime[:,0].to(device).view(-1,1); xb_prime = X_prime[:,1].to(device).view(-1,1)
+
+x = torch.stack([xr, xb], dim=0);      x_prime = torch.stack([xr_prime, xb_prime], dim=0)
+
 
 for net in neural_nets:
     net.to(device)
@@ -168,19 +186,25 @@ use_amp = True
 scaler  = torch.cuda.amp.GradScaler(enabled=use_amp)
 
 for epoch in range(epochs+1):
+    net.train()
     epoch_loss   = 0.0
+
+    net.log_parameters(epoch)
+
     for i, x in enumerate(XTrain):
 
         xr = x[:,0].view(-1,1).to(device); xb = x[:,1].view(-1,1).to(device)
+        
+        x  = torch.stack([xr, xb], dim=0)
+        if i == len(XTrain) - 1 and epoch % compute_NTK_interval != 0:
+            # get second data point set for NTK estimatino
+            x_prime  = x
 
         for i,net in enumerate(neural_nets):
-            with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=use_adaptation_algorithm):
+            # reset gradients  
+            optimizers[i].zero_grad()
 
-                net.log_parameters(epoch)
-                net.train()
-
-                # reset gradients  
-                optimizers[i].zero_grad()
+            with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=use_amp):
 
                 ### INTERIOR DOMAIN
                 # Predict interior points
@@ -207,14 +231,15 @@ for epoch in range(epochs+1):
                 ## Backward step
                 net.backward(x, U, fx, gx, use_adaption=use_adaptation_algorithm)
                 epoch_loss += net.loss.item()
-                if i == len(XTrain) - 1:
-                    x_prime  = x
 
             # Do optimisation step
-            scaler.scale(net.loss).backward()
-            scaler.step(optimizers[i])
-            scaler.update()
-
+            if use_amp:
+                scaler.scale(net.loss).backward()
+                scaler.step(optimizers[i])
+                scaler.update()
+            else:
+                net.loss.backward()
+                optimizers[i].step()
     ### END Batch loop
 
     # Compute NTK

Implementation_1/src/PDE.py
@@ -34,7 +34,7 @@ class Poisson1D:
         g   = sm.symbols('g', cls=sm.Function)(x)
 
         # Set up PDE_eqn
-        self.PDE_eqn = sm.Eq(uxx,f)
+        self.PDE_eqn = sm.Eq(uxx, f)
 
         # Set up boundary condition
         # bc_eq1 = sm.Piecewise((u, sm.Eq(x, xbc[0])),  (u, sm.Eq(x, xbc[1])), (0, True))
@@ -57,8 +57,8 @@ class Poisson1D:
 
     def compute_gradient(self, u, x):
             
-        ux   = torch.autograd.grad(u,  x, grad_outputs=torch.ones_like(u), retain_graph=True,  create_graph=True)[0]
-        uxx  = torch.autograd.grad(ux, x, grad_outputs=torch.ones_like(ux),retain_graph=True,  create_graph=True)[0]
+        ux   = torch.autograd.grad(u,  x, grad_outputs=torch.ones_like(u),   create_graph=True)[0]
+        uxx  = torch.autograd.grad(ux, x, grad_outputs=torch.ones_like(ux),  create_graph=True)[0]
 
         return torch.hstack([u, ux, uxx]).T
 
@@ -125,10 +125,10 @@ class ViscidBurger1D:
         self.ic_residual  = sm.lambdify([[self.x, self.t], self.U, self.h], ic_residual,  modules=sympyTorchmodules)      
 
     def compute_gradient(self, u, x):
-        ux_ut   = torch.autograd.grad(u,  x, grad_outputs=torch.ones_like(u),   retain_graph=True,  create_graph=True)[0]    
+        ux_ut   = torch.autograd.grad(u,  x, grad_outputs=torch.ones_like(u),   create_graph=True)[0]    
         ux   = ux_ut[:,0].view(-1,1)
         ut   = ux_ut[:,1].view(-1,1)
-        uxx_uxt  = torch.autograd.grad(ux, x, grad_outputs=torch.ones_like(ux), retain_graph=True,  create_graph=True)[0]
+        uxx_uxt  = torch.autograd.grad(ux, x, grad_outputs=torch.ones_like(ux), create_graph=True)[0]
         uxx = uxx_uxt[:,0].view(-1,1)
 
         return torch.hstack([u, ux, ut, uxx]).T

Implementation_1/src/PINN.py
@@ -14,27 +14,33 @@ class PINN(nn.Module):
         self.dtype  = dtype
         self.device = device
 
+        self.enable_parameter_log = log_parameters
+        self.enable_NTK_log       = log_NTK
+
         # initialize values for nn
         self.xin        = input_size
         self.xout       = output_size
         self.neurons    = torch.tensor(neurons)
+        
+        # single activation function for whole network
+        self.activation = nn.Tanh()      
 
         # Define layers of network
         self.input_layer      = nn.Linear(input_size, self.neurons[0], dtype=dtype,  device=device)
         layers = [self.input_layer]
+        layers.append(self.activation)
+
         if len(neurons) > 1:
             for i, neuron in enumerate(self.neurons[1:]):
                 layer = nn.Linear(self.neurons[i-1], neuron, dtype=dtype, device=device)
                 layers.append(layer)
+                layers.append(self.activation)
 
         self.output_layer     = nn.Linear(self.neurons[-1], output_size, dtype=dtype, device=device)
         layers.append(self.output_layer)
 
-        self.layers = nn.ModuleList(layers)
+        self.layers = nn.Sequential(*layers)
         self.n_layers = len(self.layers)
-        
-        # single activation function for whole network
-        self.activation = nn.Tanh()      
 
         # Initialize weights of the network
         self.apply(self._init_weights)
@@ -69,8 +75,8 @@ class PINN(nn.Module):
         if isinstance(module, nn.Linear):
             nn.init.xavier_normal_(module.weight.data)    
             if module.bias is not None:
-                nn.init.normal_(module.bias.data)            
-
+                nn.init.uniform_(module.bias.data)
+                
     def log_parameters(self, epoch):
         params = {k: v.detach().clone() for k, v in self.named_parameters()}
 
@@ -86,30 +92,28 @@ class PINN(nn.Module):
         self.network_parameters_log[epoch] = {'weight': weights, 'bias':bias}
 
     def forward(self, x):
-
-        for i, layer in enumerate(self.layers):
-            x = layer(x)
-            if i < self.n_layers - 1:
-                x = self.activation(x)
-
+        x = self.layers(x)
         return x
     
     def backward(self, X, U, f=None, g=None, h=None, use_adaption=False):
 
-        if X.shape[0] == 3:
+        if isinstance(X, list) and isinstance(U, list):
+            N = len(X);    M = len(U)
+        elif isinstance(X, torch.Tensor) and isinstance(U, torch.Tensor):
+            N = X.shape[0];   M = U.shape[0]
+
+        if N == 3:
             xr = X[0].T
             xb = X[1].T
             xi = X[2].T
-            
-        elif X.shape[0] == 2:
+        elif N == 2:
             xr = X[0].T
             xb = X[1].T
-
-        if U.shape[0] == 3:
+        if M == 3:
             U_x = U[0]
             U_xb = U[1]
             U_xi = U[2]
-        elif U.shape[0] == 2:
+        elif M == 2:
             U_x  = U[0]
             U_xb = U[1]
 
@@ -119,9 +123,9 @@ class PINN(nn.Module):
             lambda_1 = 1.0
             if use_adaption:
                 lambda_1 = self.lambda_adaptation[0]
-
-            residual        = self.pde_residual(xr, U_x, f).T
-            self.pde_loss   = lambda_1 * torch.mean(residual**2)
+                
+            residual        = self.pde_residual(xr, U_x, f)
+            self.pde_loss   = lambda_1 * torch.mean(torch.square(residual))
         
             loss.append(self.pde_loss)
 
@@ -131,8 +135,8 @@ class PINN(nn.Module):
             if use_adaption:
                 lambda_2 = self.lambda_adaptation[1]
 
-            residual        = self.bc_residual(xb, U_xb, g).T
-            self.bc_loss    = lambda_2*torch.mean(residual**2)
+            residual        = self.bc_residual(xb, U_xb, g)
+            self.bc_loss    = lambda_2 * torch.mean(torch.square(residual))
         
             loss.append(self.bc_loss)   
 
@@ -140,18 +144,16 @@ class PINN(nn.Module):
             lambda_3 = 1.0
             if use_adaption:
                 lambda_3 = self.lambda_adaptation[2]
-            residual        = self.ic_residual(xi, U_xi, h).T
-            self.ic_loss    = lambda_3*torch.mean(residual**2)
+
+            residual        = self.ic_residual(xi, U_xi, h)
+            self.ic_loss    = lambda_3*torch.mean(torch.square(residual))
             loss.append(self.ic_loss)
 
-        loss = torch.stack(loss, dim=0).sum()
+        self.train_loss = torch.stack(loss, dim=0)
+
+        self.loss = self.train_loss.sum()
         
-        self.loss = loss
-
     def compute_jacobian(self, x, grad_fn, residual_fn):
-
-        # create function for use to flatten array
-        flatten_grad = lambda row: torch.cat([elem.reshape(-1) for elem in row]).view(1,-1)
 
         device = self.device
         dtype  = self.dtype
@@ -162,30 +164,41 @@ class PINN(nn.Module):
         
         #### Compute LHS of PDE equation and set the rhs to zero
         rhs = torch.zeros((x.shape[0],1), dtype=dtype, device=device).T
-        lhs = residual_fn(x.T, y_x, rhs)
+        lhs = residual_fn(x.T, y_x, rhs).view(-1)
+        N   = lhs.shape[0]
+        I_N = torch.eye(N, dtype=dtype, device=device)
 
         #### Compute the Jacobian
-        J_y = []
-        for i in range(len(x)):
-            row = flatten_grad(torch.autograd.grad(lhs[:,i], self.parameters(), grad_outputs=torch.ones_like(lhs[:,i]), retain_graph=True))
-            J_y.append(row.detach().clone())
-        # End jacobian computation over parameters
+        if N <= 1000:
+            flatten_grad = lambda col: torch.hstack([elem.detach().view(N,-1) for elem in col])
+            J_y = flatten_grad(torch.autograd.grad(lhs, self.parameters(), I_N, is_grads_batched=True))
+        else:
+            def compute_vjp(v):
+                row = torch.autograd.grad(lhs, self.parameters(), grad_outputs=v, retain_graph=True)
+                return row
+            J_y = torch.vmap(compute_vjp, chunk_size=200)(I_N)
+            J_y = torch.hstack([col.detach().view(N,-1) for col in J_y])
 
-        return torch.vstack(J_y)
+        return J_y
 
     def NTK(self, X1, X2):
 
         PDE_K = False; BC_K = False;    IC_K = False
 
-        if X1.shape[0] == 3 and X2.shape[0] == 3:
-            xr1 = X1[0, :];   xb1 = X1[1, :];   xi1 = X1[2, :]
-            xr2 = X2[0, :];   xb2 = X2[1, :];   xi2 = X2[2, :]
-        elif X1.shape[0] == 2 and X2.shape[0] == 2:
-            xr1 = X1[0, :];   xb1 = X1[1, :]
-            xr2 = X2[0, :];   xb2 = X2[1, :]
+        if isinstance(X1, list) and isinstance(X2, list):
+            N1 = len(X1);    N2 = len(X2)
+        elif isinstance(X1, torch.Tensor) and isinstance(X2, torch.Tensor):
+            N1 = X1.shape[0];   N2 = X2.shape[0]
+        
+        if N1 == 3 and N2 == 3:
+            xr1 = X1[0];   xb1 = X1[1];   xi1 = X1[2]
+            xr2 = X2[0];   xb2 = X2[1];   xi2 = X2[2]
+        elif N1 == 2 and N2 == 2:
+            xr1 = X1[0];   xb1 = X1[1]
+            xr2 = X2[0];   xb2 = X2[1]
         else:
-            xr1 = xb1 = X1
-            xr2 = xb2 = X2
+            xr1 = X1 
+            xr2 = X2 
 
         if hasattr(self, 'pde_residual'):
 
@@ -196,10 +209,10 @@ class PINN(nn.Module):
 
             # compute NTK matrix for PDE residual
 
-            self.Krr            = torch.matmul(J_r1, J_r2.T)
+            self.Krr            = torch.matmul(J_r1, J_r2.T) 
             self.lambda_Krr, _  = torch.linalg.eig(self.Krr)
-        # endif
 
+        # endif
         if hasattr(self, 'bc_residual'):
 
             BC_K = True   
@@ -226,7 +239,7 @@ class PINN(nn.Module):
         if PDE_K and BC_K and IC_K:
             K1 = torch.vstack((J_u1,   J_r1, J_i1))
             K2 = torch.hstack((J_u2.T, J_r2.T, J_i2.T))
-            self.K = torch.matmul(K1, K2)
+            self.K = torch.matmul(K1, K2) 
             self.lambda_K,_ = torch.linalg.eig(self.K)
         elif PDE_K and BC_K:
             K1 = torch.vstack((J_u1,   J_r1))
@@ -235,6 +248,7 @@ class PINN(nn.Module):
             self.K = torch.matmul(K1, K2)
 
             self.lambda_K, _ = torch.linalg.eig(self.K)
+
         # endif
 
         # update adaption terms
@@ -242,13 +256,17 @@ class PINN(nn.Module):
 
         if PDE_K:
             Krr_trace = torch.trace(self.Krr)
+            # print(K_trace, Krr_trace)
             self.lambda_adaptation[0] = K_trace / Krr_trace
         if BC_K:
             Kuu_trace = torch.trace(self.Kuu)
+            # print(K_trace, Kuu_trace)
             self.lambda_adaptation[1] = K_trace / Kuu_trace
         if IC_K:
             Kii_trace = torch.trace(self.Kii)
-            self.lambda_adaptation[2] = K_trace / Kii_trace
+            self.lambda_adaptation[2] = K_trace / Kii_trace 
+
+        # self.lambda_adaptation = torch.clip(self.lambda_adaptation, min=1.)
 
     def log_NTK(self, epoch):
         NTK_matrix = []
@@ -285,7 +303,12 @@ class PINN(nn.Module):
         else:
             f = h5py.File(filename, mode='w')
 
-        logs = [self.network_parameters_log, self.NTK_log]
+        logs = [] 
+
+        if hasattr(self, 'network_parameters_log'):
+            logs.append(self.network_parameters_log)
+        if hasattr(self, 'NTK_log'):
+            logs.append(self.NTK_log)
 
         for log in logs:
             for epoch in log:
@@ -302,11 +325,12 @@ class PINN(nn.Module):
                         else:
                             grp[str(i)][:] = new_item
         if reset:
-            self.network_parameters_log = {}
-            self.NTK_log = {}
-
+            if hasattr(self, 'network_parameters_log'):
+                self.network_parameters_log = {}
+                logs.append(self.network_parameters_log)
+            if hasattr(self, 'NTK_log'):
+                self.NTK_log = {}
         f.close()
-
 
     def read_log(self, pathfile):
         # read file
@@ -330,14 +354,16 @@ class PINN(nn.Module):
                     NTK_matrix = row
                 elif group == "NTK_eigenvalues":
                     NTK_eigenvalues = row
-                    
+            
             epoch = int(epoch)
             epochs.append(epoch)
             network_parameters_log[epoch] = {'weight': weights, 'bias':bias}
-            NTK_log[epoch] = {'NTK_matrix': NTK_matrix, 
+            if self.enable_NTK_log:
+                NTK_log[epoch] = {'NTK_matrix': NTK_matrix, 
                                         'NTK_eigenvalues': NTK_eigenvalues}
         epochs.sort()
         self.network_parameters_log = {epoch_i: network_parameters_log[epoch_i] for epoch_i in epochs}
-        self.NTK_log                = {epoch_i: NTK_log[epoch_i] for epoch_i in epochs}
+        if self.enable_NTK_log:
+            self.NTK_log                = {epoch_i: NTK_log[epoch_i] for epoch_i in epochs}
 
         f.close()

Implementation_1/src/plotFunctions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-from src.NTK_helper import compute_parameter_diff, compute_NTK_diff, compute_convergence_rate
+from NTK_helper import compute_parameter_diff, compute_NTK_diff, compute_convergence_rate
 
 
 def plot_results1D(xplot, u_pred, u_exact, train_losses):
@@ -80,24 +80,37 @@ def plot_results2D(xplot, u_pred, Intervals, train_losses):
 
 def plot_NTK(net, fig=None, axs=None):
 
-    if fig == None and axs == None:
-        if len(net.NTK_log[0]['NTK_eigenvalues']) == 4:
-            fig, axs = plt.subplots(1,4, figsize=(23,6))
+    n_lambda = len(net.NTK_log[0]['NTK_eigenvalues']) 
+    epochs = list(net.NTK_log.keys())
+    last_epoch = epochs[-1]
 
-        if len(net.NTK_log[0]['NTK_eigenvalues']) == 3:
+    # create figure
+    if fig == None and axs == None:
+        if n_lambda == 4:
+            fig, axs = plt.subplots(1,4, figsize=(23,6))
+        elif n_lambda == 3:
             fig, axs = plt.subplots(1,3, figsize=(23,6))
 
+    # set array
+    if n_lambda >= 3:
+        lambda_K   = net.NTK_log[last_epoch]['NTK_eigenvalues'][0].cpu().numpy()
+        lambda_Krr = net.NTK_log[last_epoch]['NTK_eigenvalues'][1].cpu().numpy()
+        lambda_Kuu = net.NTK_log[last_epoch]['NTK_eigenvalues'][2].cpu().numpy()
+    if n_lambda >= 4:
+        lambda_Kii = net.NTK_log[last_epoch]['NTK_eigenvalues'][3].cpu().numpy()
+
+
     if hasattr(net, 'lambda_K'):
-        eig_K_plot    = np.sort(np.real(net.lambda_K.detach().cpu().numpy()))[::-1]
+        eig_K_plot    = np.sort(np.real(lambda_K))[::-1]
         axs[0].semilogx(eig_K_plot,      label=r'$\lambda_{K}$');     axs[0].set_title('Eigenvalue of K')
     if hasattr(net, 'lambda_Kuu'):
-        eig_K_uu_plot = np.sort(np.real(net.lambda_Kuu.detach().cpu().numpy()))[::-1]
+        eig_K_uu_plot    = np.sort(np.real(lambda_Kuu))[::-1]
         axs[1].semilogx(eig_K_uu_plot,   label=r'$\lambda_{uu}$');    axs[1].set_title('Eigenvalue of {}'.format(r"$K_{uu}$"))
     if hasattr(net, 'lambda_Krr'):
-        eig_K_rr_plot = np.sort(np.real(net.lambda_Krr.detach().cpu().numpy()))[::-1]
+        eig_K_rr_plot    = np.sort(np.real(lambda_Krr))[::-1]
         axs[2].semilogx(eig_K_rr_plot,   label=r'$\lambda_{rr}$');    axs[2].set_title('Eigenvalue of {}'.format(r"$K_{rr}$"))
     if hasattr(net, 'lambda_Kii'):
-        eig_K_ii_plot = np.sort(np.real(net.lambda_Kii.detach().cpu().numpy()))[::-1]
+        eig_K_ii_plot    = np.sort(np.real(lambda_Kii))[::-1]
         axs[3].semilogx(eig_K_ii_plot,   label=r'$\lambda_{ii}$');    axs[3].set_title('Eigenvalue of {}'.format(r"$K_{ii}$"))
 
     for ax in axs: